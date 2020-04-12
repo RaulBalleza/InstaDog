78a92d0709a6b77f8b206704b182576b1e5b3e0f
Adrian Amaya <1730156@upv.edu.mx>
2020-04-12 15:41:01 -0500

    Muestra de lista de usuarios

---
+++ app/src/main/java/tech/rauballeza/instadog/UsersFragment.java
@@ -0,0 +1,86 @@
+package tech.rauballeza.instadog;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsersFragment extends Fragment {
+
+    RecyclerView recyclerView;
+    AdapterUsers adapterUsers;
+    List<ModelUser> userList;
+
+    public UsersFragment(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_users, container, false);
+
+        //Init recyclerview
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        //init user list
+        userList = new ArrayList<>();
+
+        //getall users
+        getAllUsers();
+
+        return view;
+    }
+
+    private void getAllUsers() {
+        //get current users
+        final FirebaseUser fUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        //get path of database named "USers" containing users info
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+        //get all data from path
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                userList.clear();
+                for (DataSnapshot ds: dataSnapshot.getChildren()){
+                    ModelUser modelUser = ds.getValue(ModelUser.class);
+
+                    //get all users except currently signed in user
+                    if(!modelUser.getUid().equals(fUser.getUid())){
+                        userList.add(modelUser);
+                    }
+
+                    //adapter
+                    adapterUsers = new AdapterUsers(getActivity(), userList);
+                    //set adapter to recycler view
+                    recyclerView.setAdapter(adapterUsers);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
